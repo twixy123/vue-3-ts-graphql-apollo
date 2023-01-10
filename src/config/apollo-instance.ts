4669786541aefe42e5db199bd970e9b152e61b82
ruslan.g <luckytwixy321@gmail.com>
2023-01-10 13:39:58 +0500

    set instance via apollo-boost

--- src/config/apollo-instance.ts
+++ src/config/apollo-instance.ts
@@ -1,21 +1,6 @@
-import { ApolloClient } from 'apollo-client'
-import type { ApolloClientOptions } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
-// HTTP connection to the API
-const link = createHttpLink({
-  // You should use an absolute URL here
-  uri: 'https://graphqlzero.almansi.me/api',
-})
-
-// Cache implementation
-const cache = new InMemoryCache()
-
-const options: ApolloClientOptions<any> = {
-  cache,
-  link
-}
+import ApolloClient from 'apollo-boost'
 
 // Create the apollo client
-export const apolloClient = new ApolloClient(options)
+export const apolloClient = new ApolloClient({
+  uri: 'https://graphqlzero.almansi.me/api'
+})
